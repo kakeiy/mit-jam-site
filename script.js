5fc81d6e034a9300362559e56f384624fd86da97
kakeiy <g.tea.kay@gmail.com>
2026-05-10 01:39:11 -0400

    Refine MoMA-inspired JAM site layout

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const header = document.querySelector(".site-header");
+const header = document.querySelector(".museum-header, .site-header");
 
 window.addEventListener("scroll", () => {
+  if (!header) return;
   header.toggleAttribute("data-scrolled", window.scrollY > 8);
 });
